9aab9926221fb956ba0a5294c8d7cce5f3088705
dnsnwb <dnsnwb@mst.edu>
2019-04-11 13:28:56 -0500

    pictures displaying

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import imdb
 from imdb import IMDb
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import tmdbsimple as tmdb
+import json
 tmdb.API_KEY = '3aac901eabe71551bd666ed711135477'
 
 app = Flask(__name__)
 movie_api = IMDb()
+data = {}
+data['movie'] = []
+
 
 @app.route('/')
 def home():
@@ -21,6 +25,7 @@ def search():
     if request.method == 'POST':
         out_data = list()
         url_data = list()
+        data['movie'].clear()
 
         search_query = request.form['search']
 
@@ -29,6 +34,10 @@ def search():
         for s in search.results:
             out_data.append(s['title'])
             url_data.append(s['poster_path'])
+            data['movie'].append({
+                'title': s['title'],
+                'url': s['poster_path'],
+            })
 
         # movies = movie_api.search_movie(search_query)
         #
@@ -38,8 +47,11 @@ def search():
         #     # film = movie_api.get_movie(id)
         #     # if 'cover url' in film:
         #     #     url_data.append(film['cover url'])
-        print(out_data)
-        print(url_data)
+        print(data['movie'])
+        with open('static/movies.json', 'w') as outfile:
+            json.dump(data, outfile)
+        # print(out_data)
+        # print(url_data)
         return flask.render_template('index.html', out_data = out_data, url_data = url_data)
     return flask.redirect('')
 
