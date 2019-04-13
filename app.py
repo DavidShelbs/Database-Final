45627d8267abc5263b2e57d7bd0e5a305f421635
RollickBoon <dnshelby712@gmail.com>
2019-04-13 14:50:17 -0500

    file upload

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import flask
 import imdb
 from imdb import IMDb
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for, send_from_directory
 import tmdbsimple as tmdb
 import json
 import os
 import sqlite3
+from werkzeug.utils import secure_filename
+
 tmdb.API_KEY = '3aac901eabe71551bd666ed711135477'
 
 app = Flask(__name__)
@@ -13,6 +15,9 @@ movie_api = IMDb()
 data = {}
 data['movie'] = []
 
+UPLOAD_FOLDER = 'static/upload'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
@@ -51,6 +56,35 @@ def search():
         else:
             return flask.render_template('index.html')
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return home()
+
+@app.route('/upload/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -112,5 +146,5 @@ if __name__ == '__main__':
     );''')
     db.close()
     app.secret_key = os.urandom(12)
-    app.run(host='0.0.0.0', port='80', debug='false')
-    # app.run(host='192.168.0.16', port='80', debug='false')
+    # app.run(host='0.0.0.0', port='80', debug='false')
+    app.run(host='192.168.0.20', port='80', debug='false')
