8faba79f03f7f86e534a05b3a48aea6ead1babaa
dnsnwb <dnsnwb@mst.edu>
2019-04-24 21:43:16 -0500

    idk

--- app.py
+++ app.py
@@ -79,9 +79,9 @@ def friends():
         else:
             return flask.render_template('friends.html')
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -91,19 +91,23 @@ def upload_file():
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
+        files = request.files.getlist('file')
+        for file in files:
+            print(file.filename)
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
         # if user does not select file, browser also
         # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            filename = filename[:-3]
-            filename += "txt"
-            print(filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return home()
+        # if file.filename == '':
+        #     flash('No selected file')
+        #     return redirect(request.url)
+        # # if file and allowed_file(file.filename):
+        # filename = secure_filename(file.filename)
+        # # filename = filename[:-3]
+        # # filename += "txt"
+        # print(filename)
+        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return home()
         #####################################################################
     return home()
 
